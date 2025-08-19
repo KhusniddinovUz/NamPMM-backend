78b614bf02851e9073eba3ffbfb50cd503a231d8
khusniddinovuz <khusniddinovuz@gmail.com>
2025-08-19 14:45:15 +0200

    create news app

---
+++ news/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(blank=True, unique=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to='articles/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base = slugify(self.title) or "article"
+            slug = base
+            i = 1
+            while Article.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                i += 1
+                slug = f"{base}-{i}"
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.title
